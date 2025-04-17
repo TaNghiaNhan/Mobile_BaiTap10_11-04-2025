d569f3b36b4ff425b7160c51ef6985f2e6445532
TaNghiaNhan <164967863+TaNghiaNhan@users.noreply.github.com>
2025-04-17 10:49:00 +0700

    Upload Video

---
+++ VideoShortFireBase/app/src/main/java/vn/iotstar/videoshortfirebase/model/VideoModel1.java
@@ -0,0 +1,55 @@
+package vn.iotstar.videoshortfirebase.model;
+
+import java.io.Serializable;
+
+public class VideoModel1 implements Serializable {
+    private String desc;
+    private String title;
+    private String url;
+    private String email;
+    private String avatarUrl;
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public VideoModel1() {
+
+    }
+}
